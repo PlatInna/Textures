5a96aa511660ff4948f4f79059801340faf988b9
Platon Yarkin <yarkinpy@gmail.com>
2021-02-10 13:34:59 -0600

    Add project files.

---
+++ Textures/Solution.cpp
@@ -0,0 +1,14 @@
+// Textures.cpp : https://www.coursera.org/ C++ Development Fundamentals: Brown Belt, Week 4.
+// Task: Implement shape classes that can be drawn on an image and that jointly own a given texture.
+//
+
+#include "Common.h"
+
+using namespace std;
+
+// Этот файл сдаётся на проверку
+// Здесь напишите реализацию необходимых классов-потомков `IShape`
+
+// Напишите реализацию функции
+unique_ptr<IShape> MakeShape(ShapeType shape_type) {
+}
